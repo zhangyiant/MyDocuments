474d3fb685851d082672fdd3e7347cd232a6361a
Yi Zhang <zhangyiant@hotmail.com>
2019-10-13 16:29:38 +0800

    Add docs.

---
+++ 小学/四年级/思维导图/上海小学四年级数学第一章 提高与复习.mm
@@ -0,0 +1,107 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1570025626345" ID="ID_269609496" MODIFIED="1570025690681" TEXT="&#x4e0a;&#x6d77;&#x5c0f;&#x5b66;&#x56db;&#x5e74;&#x7ea7;&#x6570;&#x5b66;&#x7b2c;&#x4e00;&#x7ae0; &#x63d0;&#x9ad8;&#x4e0e;&#x590d;&#x4e60;">
+<node CREATED="1570455685066" ID="ID_1897948465" MODIFIED="1570455703505" POSITION="right" TEXT="&#x9006;&#x8fd0;&#x7b97;">
+<node CREATED="1570455712773" ID="ID_1592464880" MODIFIED="1570455737034" TEXT="&#x52a0;&#x6cd5;&#x548c;&#x51cf;&#x6cd5;&#x4e92;&#x4e3a;&#x9006;&#x8fd0;&#x7b97;">
+<node CREATED="1570455822997" ID="ID_210849013" MODIFIED="1570455886947">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      120 + 50 = 170
+    </p>
+    <p>
+      170 - 50 = 120
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      50 + 120 = 170
+    </p>
+    <p>
+      170 - 120 = 50
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1570455893415" ID="ID_500282116" MODIFIED="1570455921804">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      120 + 50 = 170
+    </p>
+    <p>
+      170 - 50 = 120
+    </p>
+    <p>
+      170 - 120 = 50
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1570455737892" ID="ID_1629426934" MODIFIED="1570455759456" TEXT="&#x4e58;&#x6cd5;&#x548c;&#x9664;&#x6cd5;&#x4e92;&#x4e3a;&#x9006;&#x8fd0;&#x7b97;">
+<node CREATED="1570455980183" ID="ID_723406038" MODIFIED="1570456021764">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3 &#215; 4 = 12
+    </p>
+    <p>
+      12 &#247; 4 = 3
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      4 &#215; 3 = 12
+    </p>
+    <p>
+      12 &#247; 3 = 4
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1570456024268" ID="ID_1003061284" MODIFIED="1570456050564">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3 &#215; 4 = 12
+    </p>
+    <p>
+      12 &#247; 3 = 4
+    </p>
+    <p>
+      12 &#247; 4 = 3
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1570455775110" ID="ID_887387070" MODIFIED="1570456110047" POSITION="left" TEXT="&#x4e24;&#x4f4d;&#x6570;&#x4e58;&#x9664;&#x6cd5;">
+<node CREATED="1570456112834" ID="ID_1769683661" MODIFIED="1570456115474" TEXT="&#x7ad6;&#x5f0f;"/>
+<node CREATED="1570456115751" ID="ID_1686220815" MODIFIED="1570456123581" TEXT="&#x8ba1;&#x7b97;&#x5668;&#x4f7f;&#x7528;"/>
+</node>
+<node CREATED="1570455795027" ID="ID_902932768" MODIFIED="1570455797594" POSITION="right" TEXT="&#x5206;&#x6570;">
+<node CREATED="1570456147052" ID="ID_1069624333" MODIFIED="1570456178819" TEXT="&#x5206;&#x6210;&#x51e0;&#x4efd;&#x53d6;&#x5176;&#x4e2d;&#x51e0;&#x4efd;"/>
+<node CREATED="1570456179279" ID="ID_1015647335" MODIFIED="1570456222653" TEXT="&#x4e24;&#x4e2a;&#x6216;&#x591a;&#x4e2a;&#x4e00;&#x7ec4;&#xff0c;&#x5206;&#x6210;&#x51e0;&#x7ec4;&#xff0c;&#x53bb;&#x5176;&#x4e2d;&#x51e0;&#x7ec4;"/>
+</node>
+</node>
+</map>
